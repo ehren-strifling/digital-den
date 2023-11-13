dd1da89ff7fa37bb97b226cdb01267add6fe2cb1
ehren strifling <ehrenstrifling@gmail.com>
2023-11-13 12:34:44 -0600

    Started login branch

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,1 +1,22 @@
-'use strict'
+'use strict';
+
+/**
+ * Called whenever the sign up button is pressed.
+ * @param {Event} e 
+ */
+async function signup(e) {
+  return await login(e);
+}
+/**
+ * Called whenever the login button is pressed.
+ * @param {Event} e 
+ */
+async function login(e) {
+  removeLoginButtons();
+}
+
+function removeLoginButtons() {
+  [...document.getElementsByClassName("login")].foreach(e=>{
+    e.style.display = "none";
+  });
+}
