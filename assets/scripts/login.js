b77c00932bd4b2451cb2a54c834fe70f1cf3efea
ehren strifling <ehrenstrifling@gmail.com>
2023-11-13 13:47:50 -0600

    empty login prompt becomes visible on button click

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,22 +1,53 @@
 'use strict';
 
+const loginPrompt = document.createElement("div");
+
 /**
  * Called whenever the sign up button is pressed.
  * @param {Event} e 
  */
-async function signup(e) {
-  return await login(e);
+function signup(e) {
+  return login(e);
 }
 /**
  * Called whenever the login button is pressed.
  * @param {Event} e 
  */
-async function login(e) {
-  removeLoginButtons();
+function login(e) {
+  displayLoginPrompt();
+}
+/**
+ * Called when the login prompt should be closed
+ * @param {Event} e 
+ */
+function removeLogin(e) {
+  if (e.target===loginPrompt) {
+    removeLoginPrompt();
+  }
+}
+
+function displayLoginPrompt() {
+  loginPrompt.classList.add("active");
+}
+function removeLoginPrompt() {
+  loginPrompt.classList.remove("active");
 }
 
 function removeLoginButtons() {
-  [...document.getElementsByClassName("login")].foreach(e=>{
+  [...document.getElementsByClassName("login")].forEach(e=>{
     e.style.display = "none";
   });
 }
+
+
+
+function createLoginPrompt() {
+  loginPrompt.id = "login-prompt";
+  loginPrompt.onclick = removeLogin;
+  let rect = document.createElement("div");
+  rect.classList.add("login-prompt-div");
+  loginPrompt.appendChild(rect);
+  document.body.appendChild(loginPrompt);
+}
+
+createLoginPrompt();
