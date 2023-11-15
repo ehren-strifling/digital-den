b94ec34cc7621424e452767c527f8ff8f49d0dbf
Alejandro Pedraza <luisalejandropedrazacubillos@student.mitt.ca>
2023-11-14 22:23:15 -0600

    Almost done with the responsive design

--- assets/scripts/footer-menu.js
+++ assets/scripts/footer-menu.js
@@ -17,4 +17,18 @@ function onEvent (selector, event, callback) {
 /*------------------------------------------*/
 
 const userSection = select('.user-help-section')
+const userListItems = selectAll('.list-user-section ul')
+const arrowIcon = selectAll('.fa-arrow-down')
+
+arrowIcon.forEach((arrowIcons, index) => {
+    onEvent(arrowIcons, 'click', () => {
+        // Toggle the visibility of the associated userList
+        const userList = userListItems[index];
+        userList.style.display = (userList.style.display === 'block') ? 'none' : 'block';
+
+        console.log('Hello');
+    });
+});
+
+console.log(userList)
 
